d0265e4023096aef805f86496c19ec65de466fc6
khan13 <aabdulaxadkhan@gmail.com>
2022-06-09 20:01:14 +0500

    new style api

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,22 @@ Vue.use(VueRouter);
 
 const routes = [
     {
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
         path: "/login",
         name: "login",
         meta: { layout: "empty" },
         component: () => import("./../views/login/index"),
+    },
+    {
+        path: "/register",
+        name: "register",
+        meta: { layout: "empty" },
+        component: () => import("./../views/register/index"),
+    },
+    {
+        path: "/",
+        name: "Home",
+        meta: { layout: "main" },
+        component: Home,
     },
     {
         path: "/categories",
@@ -22,6 +29,37 @@ const routes = [
         meta: { layout: "main" },
         component: () => import("./../views/categories/index"),
     },
+    {
+        path: "/detail",
+        name: "detail",
+        meta: { layout: "main" },
+        component: () => import("./../views/detail/index"),
+    },
+    {
+        path: "/history",
+        name: "history",
+        meta: { layout: "main" },
+        component: () => import("./../views/history/index"),
+    },
+    {
+        path: "/planning",
+        name: "planning",
+        meta: { layout: "main" },
+        component: () => import("./../views/planning/index"),
+    },
+    {
+        path: "/profile",
+        name: "profile",
+        meta: { layout: "main" },
+        component: () => import("./../views/profile/index"),
+    },
+    {
+        path: "/record",
+        name: "record",
+        meta: { layout: "main" },
+        component: () => import("./../views/record/index"),
+    },
+    ,
 ];
 
 const router = new VueRouter({
