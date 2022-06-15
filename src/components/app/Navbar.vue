212aa3dee19efc9c67b41512c9f0639c082ec27b
khan13 <aabdulaxadkhan@gmail.com>
2022-06-15 17:20:28 +0500

    new fuctiones isOpen

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -14,6 +14,7 @@
                         class="dropdown-trigger black-text"
                         href="#"
                         data-target="dropdown"
+                        ref="khan"
                     >
                         USER NAME
                         <i class="material-icons right">arrow_drop_down</i>
@@ -21,16 +22,20 @@
 
                     <ul id="dropdown" class="dropdown-content">
                         <li>
-                            <a href="#" class="black-text">
-                                <i class="material-icons">account_circle</i
-                                >Профиль
-                            </a>
+                            <router-link to="/profile" class="black-text">
+                                <i class="material-icons">account_circle</i>
+                                Профиль
+                            </router-link>
                         </li>
                         <li class="divider" tabindex="-1"></li>
                         <li>
-                            <a href="#" class="black-text">
-                                <i class="material-icons">assignment_return</i
-                                >Выйти
+                            <a
+                                href="#"
+                                class="black-text"
+                                @click.prevent="logout"
+                            >
+                                <i class="material-icons">assignment_return</i>
+                                Выйти
                             </a>
                         </li>
                     </ul>
@@ -39,3 +44,18 @@
         </div>
     </nav>
 </template>
+<script>
+export default {
+    methods: {
+        logout() {
+            console.log("logout");
+            this.$router.push("/login?message=logout");
+        },
+    },
+    mounted() {
+        M.Dropdown.init(this.$refs.khan, {
+            constrainWidth: true,
+        });
+    },
+};
+</script>
